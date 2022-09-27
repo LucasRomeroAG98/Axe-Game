7d5a928900ab93c1adb2aaa7e0ccc1a77bcdb949
LucasRomeroAG98 <lucasromeroag.pt@gmail.com>
2022-09-27 15:48:19 +0100

    Added axe_game.exe and a circle with AD movement on the window

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,14 +1,38 @@
 #include "raylib.h"
 int main()
 {
+  // Window dimensions
   int width{350};
   int height{200};
   InitWindow(width, height, "Axe Game");
 
+  // Circle coordinates
+
+  int circle_x{175};
+  int circle_y{100};
+  int circle_radius{25};
+
+  SetTargetFPS(60);
+
  while (WindowShouldClose() == false)
  {
   BeginDrawing();
-  ClearBackground(RED);
+  ClearBackground(WHITE);
+
+  // Game logic begins
+
+  DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+
+  if (IsKeyDown(KEY_D))
+  {
+    circle_x = circle_x + 10;
+  }
+
+  if (IsKeyDown(KEY_A))
+  {
+    circle_x = circle_x - 10;
+  }
+  // Game logic ends
   EndDrawing();
  }
 }
